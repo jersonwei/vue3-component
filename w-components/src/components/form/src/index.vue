9caec8455e65da49a577e9f26d5a7b52aeab6c60
jesonwei <872937094@qq.com>
2023-02-13 17:21:17 +0800

    ✨ feat(form): formItem compon

--- w-components/src/components/form/src/index.vue
+++ w-components/src/components/form/src/index.vue
@@ -1,7 +1,79 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, PropType, ref, watch } from 'vue'
+import { FormOptions } from './type/types'
+import cloneDeep from 'lodash/cloneDeep'
+let props = defineProps({
+  options: {
+    type: Array as PropType<FormOptions[]>,
+    required: true
+  }
+})
+let model = ref<any>(null)
+let rules = ref<any>(null)
+// 初始化表单
+const initForm = () => {
+  let m: any = {}
+  let r: any = {}
+  props.options.map((item: FormOptions) => {
+    m[item.prop!] = item.value
+    r[item.prop!] = item.rules as any
+  })
+  model.value = cloneDeep(m)
+  rules.value = cloneDeep(r)
+}
+onMounted(() => {
+  initForm()
+})
+watch(
+  () => props.options,
+  (val: FormOptions[]) => {
+    initForm()
+  },
+  { deep: true }
+)
+</script>
 <template>
-  <div>
-    form
-  </div>
+  <el-form
+    v-if="model"
+    :model="model"
+    :rules="rules"
+    v-bind="$attrs"
+    :validate-on-rule-change="false"
+  >
+    <template v-for="(item, index) in options" :key="index">
+      <el-form-item
+        v-if="!item.children || !item.children!.length"
+        :label="item.label"
+        :prop="item.prop"
+      >
+        <component
+          v-model="model[item.prop!]"
+          v-bind="item.attrs"
+          :is="`el-${item.type}`"
+        ></component>
+      </el-form-item>
+      <el-form-item
+        v-if="item.children && item.children.length"
+        :label="item.label"
+        :prop="item.prop"
+      >
+        <component
+          v-model="model[item.prop!]"
+          :placeholder="item.placeholder"
+          v-bind="item.attrs"
+          :is="`el-${item.type}`"
+        >
+          <component
+            v-for="(subItem, subIndex) in item.children"
+            :key="subIndex"
+            :is="`el-${subItem.type}`"
+            :label="subItem.label"
+            :value="subItem.value"
+          >
+          </component>
+        </component>
+      </el-form-item>
+    </template>
+  </el-form>
 </template>
 <style lang="scss" scoped></style>
--- w-components/src/views/form/index.vue
+++ w-components/src/views/form/index.vue
@@ -1,5 +1,140 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { FormOptions } from '../../components/form/src/type/types'
+
+let options: FormOptions[] = [
+  {
+    type: 'input',
+    value: '',
+    label: '用户名',
+    prop: 'username',
+    attrs: {
+      clearable: true
+    },
+    rules: [
+      {
+        required: true,
+        message: '用户名不能为空',
+        trigger: 'change'
+      },
+      {
+        min: 2,
+        max: 50,
+        message: '用户名最少2位，最多50位',
+        trigger: 'change'
+      }
+    ]
+  },
+  {
+    type: 'input',
+    value: '',
+    prop: 'password',
+    label: '密码',
+    attrs: {
+      clearable: true,
+      showPassword: true
+    },
+    rules: [
+      {
+        required: true,
+        message: '密码不能为空',
+        trigger: 'change'
+      },
+      {
+        min: 7,
+        max: 15,
+        message: '密码最少7位，最多15位',
+        trigger: 'change'
+      }
+    ]
+  },
+  {
+    type: 'select',
+    value: '1',
+    placeholder: '请选择职位',
+    prop: 'role',
+    label: '职位',
+    attrs: {
+      filterable: true,
+      clearable: true
+    },
+    rules: [
+      {
+        required: true,
+        message: '职位不能为空',
+        trigger: 'change'
+      }
+    ],
+    children: [
+      {
+        type: 'option',
+        label: '经理',
+        value: '1'
+      },
+      {
+        type: 'option',
+        label: '店长',
+        value: '2'
+      },
+      {
+        type: 'option',
+        label: '助理',
+        value: '3'
+      }
+    ]
+  },
+  {
+    type: 'checkbox-group',
+    value: [],
+    prop: 'like',
+    label: '爱好',
+    rules: [
+      {
+        required: true,
+        message: '爱好不能为空',
+        trigger: 'change'
+      }
+    ],
+    children: [
+      {
+        type: 'checkbox',
+        label: '足球',
+        value: '1'
+      },
+      {
+        type: 'checkbox',
+        label: '篮球',
+        value: '2'
+      }
+    ]
+  },
+  {
+    type: 'radio-group',
+    value: [],
+    prop: 'gender',
+    label: '性别',
+    rules: [
+      {
+        required: true,
+        message: '性别不能为空',
+        trigger: 'change'
+      }
+    ],
+    children: [
+      {
+        type: 'radio',
+        label: '男',
+        value: '1'
+      },
+      {
+        type: 'radio',
+        label: '女',
+        value: '2'
+      }
+    ]
+  }
+]
+</script>
 <template>
-  <wForm></wForm>
+  <wForm :options="options" label-width="100px"></wForm>
 </template>
 <style lang="scss" scoped></style>
